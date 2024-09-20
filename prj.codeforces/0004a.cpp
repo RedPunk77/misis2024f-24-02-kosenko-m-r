1fb5187e95b7eee63dc1b037b2c395756513c3cd
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-09-20 19:30:35 +0300

    Create 0004a.cpp

---
+++ prj.codeforces/0004a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <cmath>
+#include <map>
+#include <iterator>
+#include <numeric>
+#include <random>
+
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e9;
+const double EPS = 1e-12;
+
+main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	short k;
+	cin >> k;
+	if (!(k % 2) && k >= 4) cout << "YES";
+	else cout << "NO";
+
+	return 0;
+}
