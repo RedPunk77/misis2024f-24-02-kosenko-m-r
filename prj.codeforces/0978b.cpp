4f55be1972fb229268f15d9c27e85eaee909da17
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-09-20 19:49:58 +0300

    Create 0978b.cpp

---
+++ prj.codeforces/0978b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+
+
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //fstream file("C:\\Users\\Макар\\Desktop\\liberal.txt");
+    ll n, k = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size() - 2; ++i) {
+        if (s[i] == 'x' && s[i + 1] == 'x' && s[i + 2] == 'x') ++k;
+    }
+    cout << k;
+    return 0;
+}
