00f2de206c48937231f965e73982fedc89c286c2
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-10-09 12:05:42 +0300

    Create 1926a.cpp

---
+++ prj.codeforces/1926a.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //fstream file("C:\\Users\\Макар\\Desktop\\liberal.txt");
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll c = 0;
+        string a;
+        cin >> a;
+        for (int i = 0; i < a.size(); ++i) {
+            if (a[i] == 'A') ++c;
+        }
+        if (c >= a.size() - c) cout << "A" << "\n";
+        else cout << "B" << "\n";
+    }
+    return 0;
+}
