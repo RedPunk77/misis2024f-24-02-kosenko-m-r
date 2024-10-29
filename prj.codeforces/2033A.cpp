9f823bf866a3a2e8a5fb7f5cd6f25acf874f2206
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-10-30 00:59:25 +0300

    Create 2033a.cpp

---
+++ prj.codeforces/2033A.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //fstream file("C:\\Users\\Макар\\Desktop\\liberal.txt");
+    ll t, a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        if (a % 2) cout << "Kosuke" << "\n";
+        else cout << "Sakurako" << "\n";
+    }
+    return 0;
+}
