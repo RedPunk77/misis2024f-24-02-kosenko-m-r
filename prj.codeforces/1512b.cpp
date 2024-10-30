eca74a57edf0290e3680bedb755bb44cb5c820e3
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-10-30 14:01:32 +0300

    Create 1512b.cpp

---
+++ prj.codeforces/1512b.cpp
@@ -0,0 +1,115 @@
+///⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
+///⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+///⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+///⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+///⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+///⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+///⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+///⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+///⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+///⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+///⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
+///⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
+///⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄
+///⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
+///⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using chv2 = vector <chv>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    //fstream file("C:\\Users\\Макар\\Desktop\\liberal.txt");
+    
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n, tmp = 0;
+        ll x1, y1, x2, y2;
+        cin >> n;
+        chv2 m(n, chv(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cin >> m[i][j];
+                if (m[i][j] == '*' && tmp == 0) {
+                    x1 = j;
+                    y1 = i;
+                    ++tmp;
+                }
+                else if (m[i][j] == '*' && tmp) {
+                    x2 = j;
+                    y2 = i;
+                }
+            }
+        }
+        if (x1 != x2 && y1 != y2) {
+            m[y2][x1] = '*';
+            m[y1][x2] = '*';
+        }
+        else if (x1 == x2) {
+            if (x1 == n - 1) {
+                m[y1][x1-1] = '*';
+                m[y2][x2-1] = '*';
+            }
+            else {
+                m[y1][x1 + 1] = '*';
+                m[y2][x2 + 1] = '*';
+            }
+        }
+        else if (y1 == y2) {
+            if (y1 == n - 1) {
+                m[y1-1][x1] = '*';
+                m[y2-1][x2] = '*';
+            }
+            else {
+                m[y1 + 1][x1] = '*';
+                m[y2 + 1][x2] = '*';
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << m[i][j]; 
+            }
+            cout << "\n";
+        }
+        //cout << x1 << " " << y1 << "\n";
+        //cout << x2 << " " << y2 << "\n";
+    }
+    return 0;
+}
