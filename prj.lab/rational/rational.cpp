70d4fc2ce5ee0eff1f04496bd4e2b272e070e006
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-11-12 18:35:42 +0300

    Update rational.cpp

--- prj.lab/rational/rational.cpp
+++ prj.lab/rational/rational.cpp
@@ -21,27 +21,38 @@
 #include <random>
 #include <chrono>
 
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using chv2 = vector <chv>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
 
 class Rational {
 private:
-    double integer;
+    //ll integer;
     double frac;
 
 public:
-    Rational(double r = 0.0, double i = 0.0) : integer(r), frac(i) {}
+    Rational(double i = 0.0) : frac(i) {}
 
-    double Getinteger() const { return integer; }
+    //ll Getinteger() const { return integer; }
     double Getfrac() const { return frac; }
 
     Rational operator+(const Rational& vpriv) const {
-        return Rational(vpriv.Getinteger() + integer, vpriv.Getfrac() + frac);
+        return Rational(vpriv.Getfrac() + frac);
     }
 
     Rational operator-(const Rational& vpriv) const {
-        return Rational(vpriv.Getinteger() - integer, vpriv.Getfrac() - frac);
+        return Rational(vpriv.Getfrac() - frac);
     }
 
-    Rational operator*(const Rational& vpriv) const {
+   /* Rational operator*(const Rational& vpriv) const {
         return Rational(vpriv.Getinteger() * integer - frac * vpriv.Getfrac(),
             vpriv.Getinteger() * frac + vpriv.Getfrac() * integer);
     }
@@ -53,14 +64,14 @@ public:
         }
         return Rational((integer * vpriv.Getinteger() - frac * vpriv.Getfrac()) / dop,
             (integer * vpriv.Getfrac() + frac * vpriv.Getinteger()) / dop);
-    }
+    }*/
 
     void print() const {
         if (frac >= 0) {
-            std::cout << integer << "." << frac;
+            std::cout <<frac;
         }
         else {
-            std::cout << integer << "." << -frac;
+            std::cout <<-frac;
         }
     }
 };
@@ -76,14 +87,14 @@ int getInteger(const std::string& prompt) {
         input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
 
         //Проверка, содержит ли строка только цифры
-        if (input.empty() or !std::all_of(input.begin(), input.end(), ::isdigit)) {
-            std::cerr << "Ошибка: введено некорректное значение. Введите целое число" << std::endl;
-            continue; //Запрашиваем ввод
-        }
+        //if (input.empty() or !std::all_of(input.begin(), input.end(), ::isdigit)) {
+        //    std::cerr << "Ошибка: введено некорректное значение. Введите целое число" << std::endl;
+        //    continue; //Запрашиваем ввод
+        //}
 
         //Преобразование строки в целое число
         try {
-            return std::stoi(input); //Преобразование в целое число
+            return std::stod(input); //Преобразование в целое число
         }
         catch (const std::invalid_argument&) {
             std::cerr << "Ошибка: введено некорректное значение. Введите целое число" << std::endl;
@@ -97,15 +108,12 @@ int getInteger(const std::string& prompt) {
 int main() {
     setlocale(LC_ALL, "RUS");
 
-    int integer1 = getInteger("Введите целую часть 1-ого числа: ");
-    int frac1= getInteger("Введите дробную часть 1-ого числа: ");
-    Rational number1(integer1, frac1);
+    ll frac1 = getInteger("Введите 1е число: ");
+    Rational number1(frac1);
+    ll frac2 = getInteger("Введите 2е число: ");
+    Rational number2(frac2);
 
-    int integer2 = getInteger("Введите целую часть 2-ого числа: ");
-    int frac2 = getInteger("Введите дробную часть 2-ого числа: ");
-    Rational number2(integer2, frac2);
-
-    Rational sum = number1 + number2;
+    Rational sum = number1 + number2;  
     std::cout << "Сумма чисел: ";
     sum.print();
     std::cout << "\n";
@@ -115,12 +123,12 @@ int main() {
     diff.print();
     std::cout << "\n";
 
-    Rational mult = number1 + number2;
+    /*Rational mult = number1 * number2;
     std::cout << "Произведение чисел: ";
     mult.print();
-    std::cout << std::endl;
+    std::cout << std::endl;*/
 
-    try {
+    /*try {
         Rational Joy_Division = number1 / number2;
         std::cout << "Частное: ";
         Joy_Division.print();
@@ -128,7 +136,7 @@ int main() {
     }
     catch (const std::runtime_error& error) {
         std::cerr << error.what() << std::endl;
-    }
+    }*/
 
     return 0;
 }
