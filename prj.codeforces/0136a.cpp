60e44a70da4c615d0d32f33b55ecd42fd54f7951
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-11-13 13:09:40 +0300

    Create 0136a.cpp

---
+++ prj.codeforces/0136a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <stack>
+#include <list>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <iterator>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <chrono>
+ 
+using namespace std;
+using vll = vector <long long>;
+using vll2 = vector <vll>;
+using chv = vector <char>;
+using chv2 = vector <chv>;
+using ull = uint64_t;
+using ll = int64_t;
+using ui = uint32_t;
+using ush = uint16_t;
+const ll INF = 1e18;
+const double EPS = 1e-12;
+ 
+ 
+int main() {
+ 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    //fstream file("C:\\Users\\Макар\\Desktop\\liberal.txt");
+    
+    ll n;
+    cin >> n;
+    vector <pair <ll, ll >> v(n);
+    for (int i = 0; i< n; ++i) {
+        cin>>v[i].first;
+        v[i].second = i;
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; ++i) cout<<v[i].second + 1<<" ";
+    return 0;
+}
