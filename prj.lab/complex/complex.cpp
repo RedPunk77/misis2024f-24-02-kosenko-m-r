e41cc3990130a7cf78c1022aa2effe6aed948458
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-11-27 01:21:38 +0300

    Update complex.cpp

--- prj.lab/complex/complex.cpp
+++ prj.lab/complex/complex.cpp
@@ -1,114 +1,89 @@
-#include <iostream>
-#include <algorithm>
-#include <bitset>
-#include <iomanip>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <typeinfo>
-#include <iostream>
-#include <stack>
-#include <list>
-#include <queue>
-#include <deque>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <iterator>
-#include <cmath>
-#include <numeric>
-#include <random>
-#include <chrono>
+#pragma once
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
 
-class Complex {
-private:
-    double real;
-    double image;
-public:
-    Complex(double r = 0.0, double i = 0.0) : real(r), image(i) {}
-    double GetReal() const { return real; }
-    double GetImage() const { return image; }
-    Complex operator+(const Complex& vpriv) const {
-        return Complex(vpriv.GetReal() + real, vpriv.GetImage() + image);
-    }
-    Complex operator-(const Complex& vpriv) const {
-        return Complex(vpriv.GetReal() - real, vpriv.GetImage() - image);
-    }
-    Complex operator*(const Complex& vpriv) const {
-        return Complex(vpriv.GetReal() * real - image * vpriv.GetImage(),
-            vpriv.GetReal() * image + vpriv.GetImage() * real);
-    }
-    Complex operator/(const Complex& vpriv) const {
-        double dop = (vpriv.GetReal() * vpriv.GetReal() + vpriv.GetImage() * vpriv.GetImage());
-        if (dop == 0) {
-            throw std::runtime_error("Division by zero");
-        }
-        return Complex((real * vpriv.GetReal() - image * vpriv.GetImage()) / dop,
-            (real * vpriv.GetImage() + image * vpriv.GetReal()) / dop);
-    }
-    void print() const {
-        if (image >= 0) {
-            std::cout << real << " + " << image << "i";
-        }
-        else {
-            std::cout << real << " - " << -image << "i";
-        }
-    }
+#include <sstream>
+#include <iostream>
+#include <iosfwd>
+#include <limits>
+#include <cmath>
+#include <stdexcept>
+#include <string>
+
+struct Complex {
+    Complex() = default;
+    Complex(const Complex&) = default;
+    Complex(Complex&&) = default;
+
+    explicit Complex(const double real) : Complex(real, 0.0) {}
+    Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
+
+    ~Complex() = default;
+
+    Complex& operator=(const Complex& rhs) = default;
+    Complex& operator=(Complex&&) = default;
+    Complex& operator+=(const Complex& rhs) noexcept;
+    Complex& operator-=(const Complex& rhs) noexcept;
+    Complex& operator*=(const Complex& rhs) noexcept;
+    Complex& operator/=(const Complex& rhs);
+    Complex& operator=(const double rhs)  noexcept { return operator=(Complex(rhs)); }
+    Complex& operator+=(const double rhs) noexcept { return operator+=(Complex(rhs)); }
+    Complex& operator-=(const double rhs) noexcept { return operator-=(Complex(rhs)); }
+    Complex& operator*=(const double rhs) noexcept { return operator*=(Complex(rhs)); }
+    Complex& operator/=(const double rhs) { return operator/=(Complex(rhs)); }
+    [[nodiscard]] Complex operator-() const noexcept;
+
+    [[nodiscard]] bool operator==(const Complex& rhs) const noexcept;
+    [[nodiscard]] bool operator==(const double rhs) const noexcept { return operator==(Complex(rhs)); }
+
+    [[nodiscard]] bool operator!=(const Complex& rhs) const noexcept { return !operator==(rhs); };
+    [[nodiscard]] bool operator!=(const double rhs) const noexcept { return !operator==(Complex(rhs)); }
+
+    [[nodiscard]] std::ostream& writeTo(std::ostream& ostrm) const;
+    [[nodiscard]] std::istream& readFrom(std::istream& isdtrm);
+
+    double re{ 0.0 };
+    double im{ 0.0 };
+
+    static const char leftBrace{ '{' };
+    static const char separator{ ',' };
+    static const char rightBrace{ '}' };
 };
-//Функция получения правильно введенного целого числа
-int getInteger(const std::string& prompt) {
-    std::string input;
-    while (true) {
-        std::cout << prompt;
-        std::getline(std::cin, input); //Чтение строки целиком
-        //Удаление пробелов
-        input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
-        //Проверка, содержит ли строка только цифры
-        if (input.empty() or !std::all_of(input.begin(), input.end(), ::isdigit)) {
-            std::cerr << "Ошибка: введено некорректное значение. Введите целое число" << std::endl;
-            continue; //Запрашиваем ввод
-        }
-        //Преобразование строки в целое число
-        try {
-            return std::stoi(input); //Преобразование в целое число
-        }
-        catch (const std::invalid_argument&) {
-            std::cerr << "Ошибка: введено некорректное значение. Введите целое число" << std::endl;
-        }
-        catch (const std::out_of_range&) {
-            std::cerr << "Ошибка: переполнение целочисленного типа int" << std::endl;
-        }
-    }
+
+[[nodiscard]] Complex operator+(const Complex& lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator-(const Complex& lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator*(const Complex& lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator/(const Complex& lhs, const Complex& rhs);
+[[nodiscard]] Complex operator!(const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator*(const double lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator*(const Complex& lhs, const double rhs) noexcept;
+[[nodiscard]] Complex operator-(const double lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator-(const Complex& lhs, const double rhs) noexcept;
+[[nodiscard]] Complex operator+(const double lhs, const Complex& rhs) noexcept;
+[[nodiscard]] Complex operator+(const Complex& lhs, const double rhs) noexcept;
+[[nodiscard]] Complex operator/(const double lhs, const Complex& rhs);
+[[nodiscard]] Complex operator/(const Complex& lhs, const double rhs);
+[[nodiscard]] Complex pow(const Complex& lhs, int n);
+
+//[[nodiscard]] bool operator==(const Complex& lhs, const Complex& rhs) noexcept;
+//[[nodiscard]] bool operator!=(const Complex& lhs, const Complex& rhs) noexcept;
+//[[nodiscard]] bool operator==(const Complex& lhs, const double rhs) noexcept;
+//[[nodiscard]] bool operator==(const double lhs, const Complex& rhs) noexcept;
+//[[nodiscard]] bool operator!=(const Complex& lhs, const double rhs) noexcept;
+//[[nodiscard]] bool operator!=(const double lhs, const Complex& rhs) noexcept;
+[[nodiscard]] double abs(const Complex& rhs) noexcept;
+
+bool testParse(const std::string& str);
+void test(const Complex& c1, const Complex& c2, const double c);
+
+inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs)
+{
+    return rhs.writeTo(ostrm);
 }
-int main() {
-    setlocale(LC_ALL, "RUS");
-    int real1 = getInteger("Введите действительную часть 1-ого числа: ");
-    int imaginary1 = getInteger("Введите мнимую часть 1-ого числа: ");
-    Complex number1(real1, imaginary1);
-    int real2 = getInteger("Введите действительную часть 2-ого числа: ");
-    int imaginary2 = getInteger("Введите мнимую часть 2-ого числа: ");
-    Complex number2(real2, imaginary2);
-    Complex sum = number1 + number2;
-    std::cout << "Сумма чисел: ";
-    sum.print();
-    std::cout << "\n";
-    Complex diff = number1 - number2;
-    std::cout << "Разность чисел: ";
-    diff.print();
-    std::cout << "\n";
-    Complex mult = number1 + number2;
-    std::cout << "Произведение чисел: ";
-    mult.print();
-    std::cout << std::endl;
-    try {
-        Complex Joy_Division = number1/number2;
-        std::cout << "Частное: ";
-        Joy_Division.print();
-        std::cout << std::endl;
-    }
-    catch (const std::runtime_error& error) {
-        std::cerr << error.what() << std::endl;
-    }
-    return 0;
+
+inline std::istream& operator>>(std::istream& istrm, Complex& rhs)
+{
+    return rhs.readFrom(istrm);
 }
+
+#endif
