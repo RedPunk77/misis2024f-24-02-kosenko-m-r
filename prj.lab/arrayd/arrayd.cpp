9a699983e4ac1148eafe4a7b31df59dcc645f36d
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-04 00:45:17 +0300

    Update arrayd.cpp

--- prj.lab/arrayd/arrayd.cpp
+++ prj.lab/arrayd/arrayd.cpp
@@ -1,1 +1,134 @@
+#include <arrayd/arrayd.hpp>
+
+#include <stdexcept>
+
+
+ArrayD::ArrayD(const ArrayD& rhs)
+    : size_(rhs.size_), capacity_(rhs.size_) {
+    data_ = new double[rhs.size_];
+    for (int i = 0; i < rhs.size_; ++i) {
+        *(data_ + i) = *(rhs.data_ + i);
+    }
+}
+
+ArrayD::ArrayD(const ptrdiff_t size)
+    : size_(size), capacity_(size) {
+    if (size <= 0) {
+        throw std::invalid_argument("Zero size is not allowed in ArrayD ctor");
+    }
+    data_ = new double[size] {0};
+}
+
+
+ArrayD& ArrayD::operator=(ArrayD const& rhs) {
+    if (this != &rhs) {
+        delete[] data_;
+        size_ = rhs.size_;
+        capacity_ = rhs.size_ * 2;
+        data_ = new double[capacity_] {};
+        for (int i = 0; i < rhs.size_; i++) {
+            data_[i] = rhs.data_[i];
+        }
+    }
+    return *this;
+}
+
+ArrayD::~ArrayD() {
+    delete[] data_;
+    data_ = nullptr;
+    capacity_ = 0;
+}
+
+
+void ArrayD::grow() {
+    size_t new_capacity;
+    if (capacity_ == 0) {
+        new_capacity = 1;  // Если текущая вместимость равна 0, задаем новую вместимость как 1
+    }
+    else {
+        new_capacity = capacity_ * 2;  // Иначе удваиваем вместимость
+    }
+    Resize(new_capacity);  // Изменяем размер массива до новой вместимости
+}
+
+
+void ArrayD::Resize(const std::ptrdiff_t size) {
+    if (size < 0) {
+        throw std::invalid_argument("Resizing by negative value");
+    }
+    if (size > capacity_) {
+        std::ptrdiff_t new_capacity = size * 2;
+        auto new_data = new double[new_capacity] {0};
+        for (int i = 0; i < size_; i++) {
+            new_data[i] = data_[i];
+        }
+        delete[] data_;
+        data_ = new_data;
+        capacity_ = new_capacity;
+    }
+    if (size < size_) {
+        for (std::ptrdiff_t i = size; i < size_; ++i) {
+            data_[i] = 0;
+        }
+    }
+    size_ = size;
+}
+
+double& ArrayD::operator[](const std::ptrdiff_t idx) {
+    if (idx >= size_ || idx < 0) {
+        throw std::invalid_argument("ArrayD::operator[] - invalid argument");
+    }
+    return data_[idx];
+}
+
+const double& ArrayD::operator[](const std::ptrdiff_t idx) const {
+    if (idx >= size_ || idx < 0) {
+        throw std::invalid_argument("ArrayD::operator[] - invalid argument");
+    }
+    return data_[idx];
+}
+
+ArrayD& ArrayD::operator=(ArrayD&& rhs) noexcept {
+    if (this != &rhs) {
+        std::swap(size_, rhs.size_);
+        std::swap(capacity_, rhs.capacity_);
+        std::swap(data_, rhs.data_);
+        rhs.data_ = nullptr;
+    }
+    return *this;
+}
+
+void ArrayD::Insert(const std::ptrdiff_t idx, const double val) {
+    if (idx > size_ || idx < 0) {
+        throw std::invalid_argument("ArrayD::Insert - invalid argument");
+
+    }
+    if (size_ == capacity_) {
+        grow();
+    }
+    for (std::ptrdiff_t i = size_; i > idx; --i) {
+        data_[i] = data_[i - 1];
+
+    }
+    data_[idx] = val;
+    ++size_;
+}
+
+
+void ArrayD::Remove(const std::ptrdiff_t idx) {
+
+    if (idx >= size_ || idx < 0) {
+        throw std::invalid_argument("ArrayD::Remove - invalid argument");
+    }
+    for (std::ptrdiff_t i = idx; i < size_ - 1; i++) {
+        data_[i] = data_[i + 1];
+
+    }
+
+    if (size_ == 0) {
+        throw std::underflow_error("ArrayD::Remove - cannot remove from an empty array");
+    }
+    --size_;
+
+}
 
