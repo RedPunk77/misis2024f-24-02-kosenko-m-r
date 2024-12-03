389cfa854c31e0073f94e385c232bf05697312db
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-04 00:32:23 +0300

    Update rational.hpp

--- prj.lab/rational/rational.hpp
+++ prj.lab/rational/rational.hpp
@@ -1,146 +1,73 @@
+
+#pragma once
 #ifndef RATIONAL_HPP
 #define RATIONAL_HPP
+
+#include <cstdint>
 #include <iosfwd>
-
-#include <iostream>
-#include <algorithm>
-#include <bitset>
-#include <iomanip>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <typeinfo>
-#include <iostream>
-#include <stack>
-#include <list>
-#include <queue>
-#include <deque>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <iterator>
-#include <cmath>
-#include <numeric>
-#include <random>
-#include <chrono>
-
-using namespace std;
-using vll = vector <long long>;
-using vll2 = vector <vll>;
-using chv = vector <char>;
-using chv2 = vector <chv>;
-using ull = uint64_t;
-using ll = int64_t;
-using ui = uint32_t;
-using ush = uint16_t;
-const ll INF = 1e18;
-const double EPS = 1e-12;
+#include <stdexcept>
 
 class Rational {
-private:
-    //ll integer;
-    double frac;
-
 public:
-    Rational(double i = 0.0) : frac(i) {}
+	Rational() = default;
+	Rational(const Rational&) = default;
+	explicit Rational(const std::int32_t num) noexcept : num_(num) { }
+	Rational(const std::int32_t num, const std::int32_t den);
+	~Rational() = default;
+	Rational& operator=(const Rational&) = default;
 
-    //ll Getinteger() const { return integer; }
-    double Getfrac() const { return frac; }
+	[[nodiscard]] std::int32_t num() const noexcept { return num_; }
+	[[nodiscard]] std::int32_t den() const noexcept { return den_; }
+	[[nodiscard]] bool operator==(const Rational& lhs) const noexcept { return (num_ * lhs.den_ == den_ * lhs.num_); }
+	[[nodiscard]] bool operator!=(const Rational& lhs) const noexcept { return !operator==(lhs); }
+	[[nodiscard]] bool operator<(const Rational& rhs) const noexcept;
+	[[nodiscard]] bool operator<=(const Rational& rhs) const noexcept;
+	[[nodiscard]] bool operator>(const Rational& rhs) const noexcept;
+	[[nodiscard]] bool operator>=(const Rational& rhs) const noexcept;
+	[[nodiscard]] Rational operator-() const noexcept { return { -num_, den_ }; }
 
-    Rational operator+(const Rational& vpriv) const {
-        return Rational(vpriv.Getfrac() + frac);
-    }
+	Rational& operator+=(const Rational& rhs) noexcept;
+	Rational& operator-=(const Rational& rhs) noexcept;
+	Rational& operator*=(const Rational& rhs) noexcept;
+	Rational& operator/=(const Rational& rhs);
 
-    Rational operator-(const Rational& vpriv) const {
-        return Rational(vpriv.Getfrac() - frac);
-    }
+	Rational& operator+=(const int32_t rhs) noexcept;
+	Rational& operator-=(const int32_t rhs) noexcept;
+	Rational& operator*=(const int32_t rhs) noexcept;
+	Rational& operator/=(const int32_t rhs);
 
-     Rational operator*(const Rational& vpriv) const {
-         return Rational(vpriv.Getfrac() * frac);
-     }
 
-     Rational operator/(const Rational& vpriv) const {
-         double dop = ( vpriv.Getfrac());
-         if (dop == 0) {
-             throw std::runtime_error("Division by zero");
-         }
-         return Rational(vpriv.Getfrac() / frac);
-     }
+	std::ostream& WriteTo(std::ostream& ostrm) const noexcept;
+	std::istream& ReadFrom(std::istream& istrm) noexcept;
 
-    void print() const {
-        if (frac >= 0) {
-            std::cout << frac;
-        }
-        else {
-            std::cout << -frac;
-        }
-    }
+	static const char separator{ '/' };
+
+private:
+	std::int32_t num_ = 0;
+	std::int32_t den_ = 1;
+
+	Rational& gcd();
 };
 
-//Функция получения правильно введенного рационального числа
-double getfrac(const std::string& prompt) {
-    std::string input;
-    while (true) {
-        std::cout << prompt;
-        std::getline(std::cin, input); //Чтение строки целиком
+[[nodiscard]] Rational operator+(const Rational& lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator-(const Rational& lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator*(const Rational& lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator/(const Rational& lhs, const Rational& rhs);
+[[nodiscard]] Rational operator+(const Rational& lhs, const int32_t rhs) noexcept;
+[[nodiscard]] Rational operator-(const Rational& lhs, const int32_t rhs) noexcept;
+[[nodiscard]] Rational operator*(const Rational& lhs, const int32_t rhs) noexcept;
+[[nodiscard]] Rational operator/(const Rational& lhs, const int32_t rhs);
+[[nodiscard]] Rational operator+(const int32_t lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator-(const int32_t lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator*(const int32_t lhs, const Rational& rhs) noexcept;
+[[nodiscard]] Rational operator/(const int32_t lhs, const Rational& rhs);
 
-        //Удаление пробелов
-        input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
-
-        //Проверка, содержит ли строка только цифры
-        //if (input.empty() or !std::all_of(input.begin(), input.end(), ::isdigit)) {
-        //    std::cerr << "Ошибка: введено некорректное значение. Введите рациональное число" << std::endl;
-        //    continue; //Запрашиваем ввод
-        //}n
-
-        //Преобразование строки в рационального число
-        try {
-            return std::stod(input); //Преобразование в рационального число
-        }
-        catch (const std::invalid_argument&) {
-            std::cerr << "Ошибка: введено некорректное значение. Введите рациональное число" << std::endl;
-        }
-        catch (const std::out_of_range&) {
-            std::cerr << "Ошибка: переполнение типа double" << std::endl;
-        }
-    }
+inline std::ostream& operator<<(std::ostream& ostrm, const Rational& Rational) noexcept {
+	return Rational.WriteTo(ostrm);
 }
 
-int main() {
-    setlocale(LC_ALL, "RUS");
-
-    double frac1 = getfrac("Введите 1е число: ");
-    Rational number1(frac1);
-    double frac2 = getfrac("Введите 2е число: ");
-    Rational number2(frac2);
-
-    Rational sum = number1 + number2;
-    std::cout << "Сумма чисел: ";
-    sum.print();
-    std::cout << "\n";
-
-    Rational diff = number1 - number2;
-    std::cout << "Модуль разности чисел: ";
-    diff.print();
-    std::cout << "\n";
-
-    Rational mult = number1 * number2;
-    std::cout << "Произведение чисел: ";
-    mult.print();
-    std::cout << std::endl;
-
-    try {
-        Rational Joy_Division = number1 / number2;
-        std::cout << "Частное: ";
-        Joy_Division.print();
-        std::cout << std::endl;
-    }
-    catch (const std::runtime_error& error) {
-        std::cerr << error.what() << std::endl;
-    }
-
-    return 0;
+inline std::istream& operator>>(std::istream& istrm, Rational& Rational) noexcept {
+	return Rational.ReadFrom(istrm);
 }
 
 #endif
