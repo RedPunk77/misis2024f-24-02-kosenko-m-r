f6390e35a7d4716850859bd6e4334d99ecef4c9a
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-04 00:30:27 +0300

    Update rational.cpp

--- prj.lab/rational/rational.cpp
+++ prj.lab/rational/rational.cpp
@@ -1,140 +1,147 @@
+
+#include "rational/rational.hpp"
 #include <iostream>
-#include <algorithm>
-#include <bitset>
-#include <iomanip>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <typeinfo>
-#include <iostream>
-#include <stack>
-#include <list>
-#include <queue>
-#include <deque>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <iterator>
-#include <cmath>
-#include <numeric>
-#include <random>
-#include <chrono>
+#include <stdexcept>
 
-using namespace std;
-using vll = vector <long long>;
-using vll2 = vector <vll>;
-using chv = vector <char>;
-using chv2 = vector <chv>;
-using ull = uint64_t;
-using ll = int64_t;
-using ui = uint32_t;
-using ush = uint16_t;
-const ll INF = 1e18;
-const double EPS = 1e-12;
-
-class Rational {
-private:
-    //ll integer;
-    double frac;
-
-public:
-    Rational(double i = 0.0) : frac(i) {}
-
-    //ll Getinteger() const { return integer; }
-    double Getfrac() const { return frac; }
-
-    Rational operator+(const Rational& vpriv) const {
-        return Rational(vpriv.Getfrac() + frac);
+Rational::Rational(const std::int32_t num, const std::int32_t den)
+    : num_(num), den_(den) {
+    if (0 == den_) {
+        throw std::invaliden_argument("Zero denumenator in Rational ctor");
     }
-
-    Rational operator-(const Rational& vpriv) const {
-        return Rational(vpriv.Getfrac() - frac);
+    if (den_ < 0) {
+        num_ = -num_;
+        den_ = -den_;
     }
-
-     Rational operator*(const Rational& vpriv) const {
-         return Rational(vpriv.Getfrac() * frac);
-     }
-
-     Rational operator/(const Rational& vpriv) const {
-         double dop = ( vpriv.Getfrac());
-         if (dop == 0) {
-             throw std::runtime_error("Division by zero");
-         }
-         return Rational(vpriv.Getfrac() / frac);
-     }
-
-    void print() const {
-        if (frac >= 0) {
-            std::cout << frac;
-        }
-        else {
-            std::cout << -frac;
-        }
-    }
-};
-
-//Функция получения правильно введенного рационального числа
-double getfrac(const std::string& prompt) {
-    std::string input;
-    while (true) {
-        std::cout << prompt;
-        std::getline(std::cin, input); //Чтение строки целиком
-
-        //Удаление пробелов
-        input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
-
-        //Проверка, содержит ли строка только цифры
-        //if (input.empty() or !std::all_of(input.begin(), input.end(), ::isdigit)) {
-        //    std::cerr << "Ошибка: введено некорректное значение. Введите рациональное число" << std::endl;
-        //    continue; //Запрашиваем ввод
-        //}n
-
-        //Преобразование строки в рационального число
-        try {
-            return std::stod(input); //Преобразование в рационального число
-        }
-        catch (const std::invalid_argument&) {
-            std::cerr << "Ошибка: введено некорректное значение. Введите рациональное число" << std::endl;
-        }
-        catch (const std::out_of_range&) {
-            std::cerr << "Ошибка: переполнение типа double" << std::endl;
-        }
-    }
+    gcd();
 }
 
-int main() {
-    setlocale(LC_ALL, "RUS");
-
-    double frac1 = getfrac("Введите 1е число: ");
-    Rational number1(frac1);
-    double frac2 = getfrac("Введите 2е число: ");
-    Rational number2(frac2);
-
-    Rational sum = number1 + number2;
-    std::cout << "Сумма чисел: ";
-    sum.print();
-    std::cout << "\n";
-
-    Rational diff = number1 - number2;
-    std::cout << "Модуль разности чисел: ";
-    diff.print();
-    std::cout << "\n";
-
-    Rational mult = number1 * number2;
-    std::cout << "Произведение чисел: ";
-    mult.print();
-    std::cout << std::endl;
-
-    try {
-        Rational Joy_Division = number1 / number2;
-        std::cout << "Частное: ";
-        Joy_Division.print();
-        std::cout << std::endl;
+Rational& Rational::gcd() {
+    int32_t a = abs(num_), b = abs(den_);
+    if (a < b) {
+        std::swap(a, b);
     }
-    catch (const std::runtime_error& error) {
-        std::cerr << error.what() << std::endl;
+    if (b == 0) {
+        num_ = 0, den_ = 1;
+        return *this;
+    };
+    while (a % b != 0) {
+        a %= b;
+        std::swap(a, b);
     }
+    num_ /= b;
+    den_ /= b;
+    return *this;
+}
 
-    return 0;
+
+
+
+bool Rational::operator<(const Rational& rhs) const noexcept {
+    return (num_ * rhs.den_ < rhs.num_ * den_);
+}
+bool Rational::operator<=(const Rational& rhs) const noexcept {
+    return (num_ * rhs.den_ <= rhs.num_ * den_);
+}
+bool Rational::operator>(const Rational& rhs) const noexcept {
+    return (num_ * rhs.den_ > rhs.num_ * den_);
+}
+bool Rational::operator>=(const Rational& rhs) const noexcept {
+    return (num_ * rhs.den_ >= rhs.num_ * den_);
+}
+
+
+Rational& Rational::operator+=(const Rational& rhs) noexcept
+{
+    num_ *= rhs.den_;
+    num_ += rhs.num_ * den_;
+    den_ *= rhs.den_;
+    return gcd();
+};
+Rational& Rational::operator-=(const Rational& rhs) noexcept 
+{
+    num_ *= rhs.den_;
+    num_ -= rhs.num_ * den_;
+    den_ *= rhs.den_;
+    return gcd();
+};
+Rational& Rational::operator*=(const Rational& rhs) noexcept 
+{
+    num_ *= rhs.num_;
+    den_ *= rhs.den_;
+    return gcd();
+};
+Rational& Rational::operator/=(const Rational& rhs) 
+{
+    if (rhs.num_ == 0) {
+        throw std::runtime_error("Division by zero");
+    }
+    num_ *= rhs.den_;
+    den_ *= rhs.num_;
+    return gcd();
+};
+
+Rational& Rational::operator+=(const int32_t rhs) noexcept 
+{
+    Rational tmp{ rhs };
+    *this += tmp;
+    return *this;
+};
+Rational& Rational::operator-=(const int32_t rhs) noexcept 
+{
+    Rational tmp{ rhs };
+    *this -= tmp;
+    return *this;
+};
+Rational& Rational::operator*=(const int32_t rhs) noexcept 
+{
+    Rational tmp{ rhs };
+    *this *= tmp;
+    return *this;
+};
+Rational& Rational::operator/=(const int32_t rhs) 
+{
+    Rational tmp{ rhs };
+    return *this /= tmp;
+};
+
+
+Rational operator+(const Rational& lhs, const Rational& rhs) noexcept { return Rational{ lhs } += rhs; }
+Rational operator-(const Rational& lhs, const Rational& rhs) noexcept { return Rational{ lhs } -= rhs; }
+Rational operator*(const Rational& lhs, const Rational& rhs) noexcept { return Rational{ lhs } *= rhs; }
+Rational operator/(const Rational& lhs, const Rational& rhs) { return Rational{ lhs } /= rhs; }
+Rational operator+(const Rational& lhs, const int32_t rhs) noexcept { return Rational{ lhs } += rhs; }
+Rational operator-(const Rational& lhs, const int32_t rhs) noexcept { return Rational{ lhs } -= rhs; };
+Rational operator*(const Rational& lhs, const int32_t rhs) noexcept { return Rational{ lhs } *= rhs; };
+Rational operator/(const Rational& lhs, const int32_t rhs) { return Rational{ lhs } /= rhs; }
+Rational operator+(const int32_t lhs, const Rational& rhs) noexcept { return operator+(rhs, lhs); }
+Rational operator-(const int32_t lhs, const Rational& rhs) noexcept { return operator-(rhs, lhs); }
+Rational operator*(const int32_t lhs, const Rational& rhs) noexcept { return operator*(rhs, lhs); }
+Rational operator/(const int32_t lhs, const Rational& rhs) { return operator/(rhs, lhs); }
+
+
+std::ostream& Rational::WriteTo(std::ostream& ostrm) const noexcept {
+    ostrm << num_ << separator << den_;
+    return ostrm;
+}
+
+std::istream& Rational::ReadFrom(std::istream& istrm) noexcept {
+    int32_t num = 0;
+    int32_t den = 1;
+    char sep = '0';
+    istrm >> num;
+    istrm.get(sep);
+    int32_t tr = istrm.peek();
+    istrm >> den;
+    if (!istrm || tr > '9' || tr < '0') {
+        istrm.setstate(std::ios_base::failbit);
+        return istrm;
+    }if (istrm.good() || istrm.eof()) {
+        if ('/' == sep && den > 0) {
+            *this = Rational(num, den);
+        }
+        else {
+            istrm.setstate(std::ios_base::failbit);
+        }
+    }
+    return istrm;
 }
