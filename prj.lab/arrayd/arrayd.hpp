5c99be66975e2fa0372e64d03acb3f75a31e6193
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-27 00:25:49 +0300

    Update arrayd.hpp

--- prj.lab/arrayd/arrayd.hpp
+++ prj.lab/arrayd/arrayd.hpp
@@ -9,37 +9,36 @@
 
 class ArrayD {
  public : 
-	 ArrayD() = default;
+   ArrayD() = default;
 
-	 ArrayD(const ArrayD&);
+   ArrayD(const ArrayD&);
 
-	 ArrayD(const std::ptrdiff_t size);
+   ArrayD(const std::ptrdiff_t size);
 
-	 ~ArrayD();
+   ~ArrayD();
 
-	 std::ptrdiff_t Size() const noexcept { return size_; }
+   std::ptrdiff_t Size() const noexcept { return size_; }
 
 
-	 ArrayD& operator=(const ArrayD& dynarr);
-	 ArrayD& operator=(ArrayD&& rhs) noexcept;
+   ArrayD& operator=(const ArrayD& dynarr);
+   ArrayD& operator=(ArrayD&& rhs) noexcept;
 
-	 //! \param idx - индекс  элемента, 0 <= idx < Size()
-	 [[nodiscard]] double& operator[](const std::ptrdiff_t idx);
-	 [[nodiscard]] const double& operator[](const std::ptrdiff_t idx) const;
 
-	 void Resize(const std::ptrdiff_t size);
+   [[nodiscard]] double& operator[](const std::ptrdiff_t idx);
+   [[nodiscard]] const double& operator[](const std::ptrdiff_t idx) const;
 
-	 //! \param idx - индекс вставляемого элемента, 0 <= idx <= size 
-	 void Insert(const std::ptrdiff_t idx, const double val);
+   void Resize(const std::ptrdiff_t size);
 
-	 //! \param idx - индекс удаляемого элемента, 0 <= idx < size 
-	 void Remove(const std::ptrdiff_t idx);
+
+   void Insert(const std::ptrdiff_t idx, const double val);
+
+  
+   void Remove(const std::ptrdiff_t idx);
 
  private:
-	std::ptrdiff_t capacity_ = 0;  //размер буффера
-	std::ptrdiff_t size_ = 0;      //число элементов в массиве
-	double* data_ = 0;
-	void grow();
+  std::ptrdiff_t capacity_ = 0; 
+  std::ptrdiff_t size_ = 0;      
+  double* data_ = 0;
 };
 
 
