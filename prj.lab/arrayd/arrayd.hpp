193be20fcbedbebd6322225d9b8b000ecdc8db46
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-27 00:37:01 +0300

    Update arrayd.hpp

--- prj.lab/arrayd/arrayd.hpp
+++ prj.lab/arrayd/arrayd.hpp
@@ -23,22 +23,23 @@ class ArrayD {
    ArrayD& operator=(const ArrayD& dynarr);
    ArrayD& operator=(ArrayD&& rhs) noexcept;
 
-
+   
    [[nodiscard]] double& operator[](const std::ptrdiff_t idx);
    [[nodiscard]] const double& operator[](const std::ptrdiff_t idx) const;
 
    void Resize(const std::ptrdiff_t size);
 
-
+  
    void Insert(const std::ptrdiff_t idx, const double val);
 
-  
+   
    void Remove(const std::ptrdiff_t idx);
 
  private:
-  std::ptrdiff_t capacity_ = 0; 
+  std::ptrdiff_t capacity_ = 0;  
   std::ptrdiff_t size_ = 0;      
   double* data_ = 0;
+  void grow();
 };
 
 
