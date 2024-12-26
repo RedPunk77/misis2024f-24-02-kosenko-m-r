a9e3a08d85a1d89a1199130566d05cfeeba215d8
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2024-12-27 00:24:47 +0300

    Update arrayd.cpp

--- prj.lab/arrayd/arrayd.cpp
+++ prj.lab/arrayd/arrayd.cpp
@@ -14,7 +14,7 @@ ArrayD::ArrayD(const ArrayD& rhs)
 ArrayD::ArrayD(const ptrdiff_t size)
     : size_(size), capacity_(size) {
     if (size <= 0) {
-        throw std::invalid_argument("Zero size is not allowed in ArrayD ctor");
+        throw std::invalid_argument("Zero size isn`t allowed in ArrayD ctor");
     }
     data_ = new double[size] {0};
 }
@@ -40,53 +40,27 @@ ArrayD::~ArrayD() {
 }
 
 
-//void ArrayD::Resize(const std::ptrdiff_t size) {
-//    if (size < 0) {
-//        throw std::invalid_argument("Resizing by negative value");
-//    }
-//    if (size > capacity_) {
-//        auto new_data = new double[size] {0, 0};
-//        if (size > 0) {
-//            for (int i = 0; i < size_; i++) {
-//                new_data[i] = data_[i];
-//            }
-//        }
-//        std::swap(data_, new_data);
-//        delete[] new_data;
-//        capacity_ = size;
-//    }
-//    else {
-//        if (size_ < size) {
-//            for (std::ptrdiff_t i = size; i < size_; ++i) {
-//                data_[i] = 0;
-//            }
-//        }
-//    }
-//
-//    size_ = size;
-//}
-
 void ArrayD::Resize(const std::ptrdiff_t size) {
     if (size < 0) {
         throw std::invalid_argument("Resizing by negative value");
     }
     if (size > capacity_) {
-        // Если новый размер больше текущей вместимости, увеличиваем массив
+      
         auto new_data = new double[size] {};
         for (int i = 0; i < size_; ++i) {
             new_data[i] = data_[i];
         }
-        delete[] data_; // Освобождаем старую память
+        delete[] data_; 
         data_ = new_data;
-        capacity_ = size; // Обновляем вместимость
+        capacity_ = size; 
     }
     else {
-        // Если новый размер меньше текущего, обнуляем "лишние" элементы
+     
         for (std::ptrdiff_t i = size; i < size_; ++i) {
             data_[i] = 0;
         }
     }
-    size_ = size; // Обновляем размер
+    size_ = size;
 }
 
 
@@ -116,30 +90,15 @@ ArrayD& ArrayD::operator=(ArrayD&& rhs) noexcept {
     return *this;
 }
 
-//void ArrayD::Insert(const std::ptrdiff_t idx, const double val) {
-//    if (idx > size_ || idx < 0) {
-//        throw std::invalid_argument("ArrayD::Insert - invalid argument");
-//
-//    }
-//        Resize(size_ + 1);
-//        if (idx != size_ - 1) {
-//            for (std::ptrdiff_t i = size_; i > idx; --i) {
-//                data_[i] = data_[i - 1];
-//
-//            }
-//        }
-//    data_[idx] = val;
-//}
-
 
 void ArrayD::Insert(const std::ptrdiff_t idx, const double val) {
     if (idx > size_ || idx < 0) {
         throw std::invalid_argument("ArrayD::Insert - invalid argument");
     }
 
-    // Увеличиваем вместимость, если массив заполнен
+  
     if (size_ == capacity_) {
-        // Расширяем массив
+       
         std::ptrdiff_t new_capacity;
         if (capacity_ == 0) {
             new_capacity = 1;
@@ -149,24 +108,24 @@ void ArrayD::Insert(const std::ptrdiff_t idx, const double val) {
         }
         double* new_data = new double[new_capacity] {};
 
-        // Копируем старые элементы
+        
         for (std::ptrdiff_t i = 0; i < size_; ++i) {
             new_data[i] = data_[i];
         }
 
-        delete[] data_;       // Освобождаем старую память
-        data_ = new_data;     // Указываем на новый массив
-        capacity_ = new_capacity; // Обновляем вместимость
+
+        delete[] data_;       
+        data_ = new_data;     
+        capacity_ = new_capacity; 
     }
 
-    // Сдвиг элементов вправо
     for (std::ptrdiff_t i = size_; i > idx; --i) {
         data_[i] = data_[i - 1];
     }
 
-    // Вставка нового элемента
+    
     data_[idx] = val;
-    ++size_; // Увеличиваем размер
+    ++size_;
 }
 
 
@@ -184,10 +143,5 @@ void ArrayD::Remove(const std::ptrdiff_t idx) {
         }
     }
 
-    //if (size_ == 0) {
-    //    throw std::underflow_error("ArrayD::Remove - cannot remove from an empty array");
-    //}
-    --size_;
 
 }
-
